80c97694550b581b439d12cfbc47d1d4878c8cfa
yogimahaputra <yogi.mahaputra123@gmail.com>
2021-06-09 12:28:12 +0700

    add login & register pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,24 @@
 var menu = document.getElementById("menu");
 
-menu.innerHTML = '<i class="fas fa-bars"></i>'
+if(menu){
+  menu.innerHTML = '<i class="fas fa-bars"></i>'
+}
 
 
 var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity( elem, {
-  // options
-  cellAlign: 'left',
-  contain: true
-});
+if(elem){
+  var flkty = new Flickity( elem, {
+    // options
+    cellAlign: 'left',
+    contain: true
+  });
+}
 
 var elem = document.querySelector('.second-carousel');
-var flkty = new Flickity( elem, {
-  // options
-  cellAlign: 'left',
-  contain: true
-});
+if(elem){
+  var flkty = new Flickity( elem, {
+    // options
+    cellAlign: 'left',
+    contain: true
+  });
+}
