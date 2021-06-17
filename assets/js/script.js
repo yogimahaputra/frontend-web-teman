dcbb490bbd5af4dcfe16aaa031aa4ae259a22686
yogimahaputra <yogi.mahaputra123@gmail.com>
2021-06-17 12:05:51 +0700

    add detail lomba

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,3 +33,16 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
 }
+
+
+function getFile() {
+  document.getElementById("upfile").click();
+}
+
+function sub(obj) {
+  var file = obj.value;
+  var fileName = file.split("\\");
+  document.getElementById("yourBtn").innerHTML = fileName[fileName.length - 1];
+  event.preventDefault();
+}
+
